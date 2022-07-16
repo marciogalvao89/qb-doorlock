8b2227b199a62e3fabba58569b1d07f714b3e190
marciogalvao89 <32371159+marciogalvao89@users.noreply.github.com>
2022-07-16 15:30:54 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,7 +10,7 @@ shared_scripts {
     'config.lua',
     'configs/*.lua',
     '@qb-core/shared/locale.lua',
-    'locales/en.lua' -- Change this to your preferred language
+    'locales/pt.lua' -- Change this to your preferred language
 }
 
 server_script 'server/main.lua'
